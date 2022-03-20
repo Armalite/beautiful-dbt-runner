07df703908f8d25331e2c26b166d344193bf4a4c
armalite <ttc_rulz@hotmail.com>
2022-03-20 23:21:43 +1300

    Updated code with error messages and more exception handling

--- src/classes/pipeline.py
+++ src/classes/pipeline.py
@@ -4,20 +4,19 @@
 """ Class representing a DBT pipeline object. This class contains all parameters and
 control variables needed to prepare the DBT pipeline for execution """
 
+import base64
 import glob
 import os
 import shutil
 import subprocess
-import time
 import sys
 import tarfile
 from os import chmod
-import select
 
 import boto3
-import requests
 import pygit2
-from botocore.exceptions import ClientError
+import requests
+from botocore.exceptions import ClientError, NoCredentialsError
 
 from src.classes.helpers import ChangeDir
 from src.classes.logger import DBTLogger
@@ -31,7 +30,6 @@ class DBTPipeline:
 
     logger = None
     _package_path = "dbt_download"
-    _log_process = None
 
     _env_vars = {}
 
@@ -45,16 +43,13 @@ class DBTPipeline:
                 # Fetch DBT package as a stream from Artifactory
                 dbt_package = requests.get(self.dbt_package_url, stream=True)
             except requests.exceptions.Timeout as e:
-                self.logger.printlog("ERROR: Request to fetch Artifactory package has timed out")
-                raise SystemExit(e)
+                self.logger.printlog(f"ERROR: Request to fetch Artifactory package has timed out. Error: {e}")
                 sys.exit(1)
             except requests.exceptions.TooManyRedirects as e:
-                self.logger.printlog("ERROR: Invalid Artifactory URL provided")
-                raise SystemExit(e)
+                self.logger.printlog(f"ERROR: Invalid Artifactory URL provided. Error: {e}")
                 sys.exit(1)
             except requests.exceptions.RequestException as e:
-                self.logger.printlog("ERROR: Could not fetch Artifactory package")
-                raise SystemExit(e)
+                self.logger.printlog(f"ERROR: Could not fetch Artifactory package. Error: {e}")
                 sys.exit(1)
 
             # Write the fetched package stream in chunks
@@ -63,7 +58,7 @@ class DBTPipeline:
                     for chunk in dbt_package.iter_content(32 * 1024):
                         f.write(chunk)
             except Exception as e:
-                self.logger.printlog(f"ERROR: Failed to save downloaded DBT Package: {e}")
+                self.logger.printlog(f"ERROR: Failed to save downloaded DBT Package. Error: {e}")
                 sys.exit(1)
 
             try:
@@ -82,7 +77,8 @@ class DBTPipeline:
                 sys.exit(1)
         else:
             self.logger.printlog(
-                "Error: DBT_PACKAGE_TYPE set to 'artifactory' but artifactory URL not set in DBT_PACKAGE_URL")
+                "ERROR: DBT_PACKAGE_TYPE set to 'artifactory' but artifactory URL not set in DBT_PACKAGE_URL")
+            sys.exit(1)
 
     def get_dbt_s3(self) -> None:
         """Fetch DBT Package from S3"""
@@ -92,31 +88,34 @@ class DBTPipeline:
         # Download the packaged dbt project from S3
         s3_client.download_file('MyBucket', self.dbt_package_url, self.dbt_path)
 
-    def get_dbt_github(self, branch=None) -> None:
-        """Fetch DBT project from Github"""              
+    def get_dbt_github(self, branch: str = None) -> None:
+        """Fetch DBT project from Github"""
         self.logger.printlog(f"Fetching DBT package from Github branch {branch} in repository: {self.dbt_package_url}")
         self.dbt_path = f"{self.package_path}/{self.dbt_path}"
         shutil.rmtree(self.dbt_path, ignore_errors=True)  # Delete folder on run
         git_repo_url = self.dbt_package_url
-        
-        try :
+
+        try:
             cloned_repo = pygit2.clone_repository(git_repo_url, self.dbt_path)
         except Exception as e:
-            self.logger.printlog(f"Could not clone the github repository. Exception: {e}")
+            self.logger.printlog(f"ERROR: Could not clone the github repository. Exception: {e}")
+            sys.exit(1)
 
         self.logger.printlog(f"DBT repository successfully cloned to {self.dbt_path}")
-        branch_name=""
+        branch_name = ""
+        branch_origin = ""
+        branch_ref = ""
         if branch:
-            try :
-                branch_name=f"origin/{branch}"
-                branch_origin=cloned_repo.branches[branch_name]
-                branch_ref=cloned_repo.lookup_reference(branch_origin.name)
-                cloned_repo.checkout(branch_ref)
+            try:
+                branch_name = f"origin/{branch}"
+                branch_origin = cloned_repo.branches[branch_name]  # type: ignore
+                branch_ref = cloned_repo.lookup_reference(branch_origin.name)  # type: ignore
+                cloned_repo.checkout(branch_ref)  # type: ignore
             except Exception as e:
-                self.logger.printlog(f"Could not checkout the branch: {branch_name}. Exception: {e}")
+                self.logger.printlog(f"ERROR: Could not checkout the branch: {branch}. Exception: {e}")
                 sys.exit(1)
-           
-            self.logger.printlog(f"Successfully checked out the following branch from cloned DBT project: {branch_name}")
+
+            self.logger.printlog(f"Successfully checked out the following branch from cloned DBT project: {branch}")
 
     def get_dbt_code(self) -> None:
         """Fetch DBT package based on the package type"""
@@ -136,8 +135,8 @@ class DBTPipeline:
 
     def get_credentials(self) -> dict:
         """Obtain the service account credentials from AWS Secrets manager
-        using the secret_arn provided. If DBT_PASS environment variable
-        has been set, the secrets fetching step is ignored """
+        using the secret_arn provided. If DBT_PASS environment variable is set and
+        the DBT_CRED_TYPE is set to 'password', then the secrets fetching step is ignored """
 
         if os.environ.get("DBT_CRED_TYPE") == "password" or os.environ.get("DBT_CRED_TYPE") is None:
             self.logger.printlog("DBT_CRED_TYPE is set to 'password'. Expecting a password for DBT credentials.")
@@ -161,18 +160,19 @@ class DBTPipeline:
                     return password_val
                 except ClientError as err:
                     if err.response["Error"]["Code"] == "AccessDeniedException":
-                        self.logger.printlog(f"Access Denied to Secrets Manager secrets: {self.dbt_pass_secret_arn}")
+                        self.logger.printlog(
+                            f"ERROR: Access Denied to Secrets Manager secrets: {self.dbt_pass_secret_arn}")
                         sys.exit(1)
                     else:
-                        self.logger.printlog(f"Unexpected error: {err}")
+                        self.logger.printlog(f"ERROR: Unexpected error. Error: {err}")
+                        sys.exit(1)
                 except NoCredentialsError as ncerr:
                     self.logger.printlog(
-                        f"AWS credentials not found. Please pass AWS Credentials to the container (access key id, secret access key, session token). Error: {ncerr}")
+                        f"ERROR: AWS credentials not found. Please pass AWS Credentials to the container (access key id, secret access key, session token). Error: {ncerr}")
                     sys.exit(1)
         elif os.environ.get("DBT_CRED_TYPE") == "key":
             self.logger.printlog(
                 f"DBT_CRED_TYPE is set to 'key'. Expecting a private key file with the following name: {self.dbt_key_name}")
-            self.logger.printlog("You must provide a dbt profile that accepts a private key path!")
             if self.dbt_pass_secret_arn is None:
                 self.logger.printlog(
                     "For DBT_CRED_TYPE of 'key', the (DBT_PASS_SECRET_ARN) environment variables must be set")
@@ -209,53 +209,52 @@ class DBTPipeline:
             except ClientError as err:
                 if err.response["Error"]["Code"] == "AccessDeniedException":
                     self.logger.printlog(
-                        f"Access Denied to Secrets Manager secrets: {self.dbt_pass_secret_arn} . Error: {err}")
-                    exit(1)
+                        f"ERROR: Access Denied to Secrets Manager secrets: {self.dbt_pass_secret_arn} . Error: {err}")
+                    sys.exit(1)
                 else:
                     self.logger.printlog(f"Unexpected error: {err}")
+                    sys.exit(1)
             except NoCredentialsError as ncerr:
                 self.logger.printlog(
-                    f"AWS credentials not found. Please pass AWS Credentials to the container (access key id, secret access key, session token). Error: {ncerr}")
-                exit(1)
-    
-    def cleanup_packages(self) -> None:
-        """Cleanup all downloaded packages in the download directory"""
-        folder = self.package_path
-        for filename in os.listdir(folder):
-            file_path = os.path.join(folder, filename)
-            try:
-                if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path)
-            except Exception as e:
-                print('Failed to delete %s. Reason: %s' % (file_path, e))
+                    f"ERROR: AWS credentials not found. Please pass AWS Credentials to the container (access key id, secret access key, session token). Error: {ncerr}")
+                sys.exit(1)
 
     def run_dbt_command(self) -> None:
         """Run the specified DBT/shell command"""
+        if os.environ.get("DBT_PASS"):
+            # Magically grant full access to the shell script(s) within the dbt folder
+            try:
+                for file in glob.glob(f'{self.dbt_path}/*.sh'):
+                    if file.endswith(".sh"):
+                        chmod(file, 777)
+                        subprocess.call(['find . -name "*.sh" -exec chmod 777 {} +'], shell=True)
+            except FileNotFoundError as err:
+                self.logger.printlog(
+                    f"ERROR: Target dbt project folder not found. Please ensure DBT_PATH is set to the name of the project folder. Error: {err}")
+                sys.exit(1)
 
-        # Magically grant full access to the shell script(s) within the dbt folder
-        try:
-            for file in glob.glob(f'{self.dbt_path}/*.sh'):
-                if file.endswith(".sh"):
-                    chmod(file, 777)
-        except FileNotFoundError as err:
+            # Switch directory context to dbt folder and run the provided shell command/script
+            try:
+                with ChangeDir(f"{self.dbt_path}"):
+                    self.logger.printlog(f"Running DBT command: {self.dbt_command}")
+                    subprocess.call([f"{self.dbt_command}"], shell=True)
+            except FileNotFoundError as err:
+                self.logger.printlog(
+                    f"ERROR: Target dbt project folder not found. Please ensure DBT_PATH is set to the name of the project folder. Error: {err}")
+                sys.exit(1)
+            except Exception as err:
+                self.logger.printlog(
+                    f"ERROR: There was a problem attempting to execute the provided shell command. Error: {err}")
+                sys.exit(1)
+        else:
             self.logger.printlog(
-                f"Target dbt project folder not found. Please ensure DBT_PATH is set to the name of the project folder. Error: {err}")
+                "WARNING: Credentials missing (DBT_PASS) due to unsuccessful secret fetch or not directly provided. Skipping execution of DBT commands...")
+            sys.exit(1)
 
-        # Switch directory context to dbt folder and run the provided shell command/script
-        try:
-            with ChangeDir(f"{self.dbt_path}"):
-                self.logger.printlog(f"Running DBT command: {self.dbt_command}")
-                subprocess.call([f"{self.dbt_command}"], shell=True)
-        except FileNotFoundError as err:
-            self.logger.printlog(
-                f"Target dbt project folder not found. Please ensure DBT_PATH is set to the name of the project folder. Error: {err}")
-
-    def add_custom_macros(self) -> None:
-        """Add custom DBT Macros to the DBT project folder"""
+    def add_xade_dbt_macros(self) -> None:
+        """Add XADE specific DBT Macros to the DBT project folder"""
         if self.register_assets:
-            self.logger.printlog(f"Adding custom DBT macros to the DBT Project folder {self.dbt_path}/")
+            self.logger.printlog(f"Adding XADE DBT macros to the DBT Project folder {self.dbt_path}/")
             src = f"{os.getcwd()}/macros/"
             dest = f"{self.dbt_path}/macros/"
             src_files = os.listdir(src)
@@ -269,14 +268,29 @@ class DBTPipeline:
         else:
             self.logger.printlog("Asset registration disabled")
 
+    def cleanup_packages(self) -> None:
+        """Cleanup all downloaded packages in the download directory"""
+        folder = self.package_path
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+                sys.exit(1)
+
     def output_dbt_logs(self) -> None:
+        """Print out the detailed dbt.log file"""
         logfile = f"{self.dbt_path}/logs/dbt.log"
         self.logger.printlog("Outputting DBT Logs")
         with open(logfile, 'r') as f:
             loglines = f.readlines()
             for line in loglines:
                 self.logger.printlog(line)
-            f.close()              
+            f.close()
 
     @property
     def env_vars(self) -> dict:
@@ -473,6 +487,11 @@ class DBTPipeline:
         self._env_vars["DBT_PACKAGE_BRANCH"] = value
 
     @property
+    def package_path(self) -> str:
+        """Get the parent path where downloaded packages will be extracted"""
+        return self._package_path
+
+    @property
     def dbt_cred_type(self) -> str:
         """Get the value of DBT_CRED_TYPE flag"""
         return self._env_vars["DBT_CRED_TYPE"]
@@ -492,15 +511,10 @@ class DBTPipeline:
         """Set the value of DBT_KEY_NAME flag"""
         self._env_vars["DBT_KEY_NAME"] = value
 
-    @property
-    def package_path(self) -> str:
-        """Get the parent path where downloaded packages will be extracted"""
-        return self._package_path
-
     def __init__(
         self,
         config: dict
     ):
         self.logger = DBTLogger()
-        self.logger.printlog("DBT Runner process started")
+        self.logger.printlog("DBT Pipeline process started")
         self.env_vars = config
